c8472a378d34c5118c3fb1d8e748ade004955c2f
Xevaquor <xevaquor@hotmail.com>
2012-09-01 21:30:20 +0200

    First attempt to integrate physics engine - somewhat works but code is awful.

--- src/dev_adventure/dev_adventure/GameObject.cs
+++ src/dev_adventure/dev_adventure/GameObject.cs
@@ -9,45 +9,69 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Factories;
+using FarseerPhysics.SamplesFramework;
 
 namespace DevAdventure
 {
     class GameObject
     {
         public Vector2 Position;
+        /// <summary>
+        /// In radians
+        /// </summary>
         public float Rotation;
         public Vector2 Velocity;
         public Vector2 Origin;
 
         public AnimatedSprite Sprite;
+
+        public Body PhysicsBody;
+        public Fixture PhysicsFixture;
         
-        public GameObject(AnimatedSprite animated_sprite, Vector2 pos, float rot, Vector2 ori, Vector2 vel)
+        public GameObject(AnimatedSprite animated_sprite, Vector2 pos, float rot, Vector2 ori, Vector2 vel, Body body)
         {
             Sprite = animated_sprite;
             Position = pos;
             Rotation = rot;
             Velocity = vel;
             Origin = ori;
+            PhysicsBody = body;
         }
         public GameObject(AnimatedSprite animated_sprite, Vector2 pos, float rot = 0.0f)
-            : this(animated_sprite, pos, rot, Vector2.Zero, Vector2.Zero)
+            : this(animated_sprite, pos, rot, Vector2.Zero, Vector2.Zero, null)
         {
             Origin = new Vector2(Sprite.Area.Width / 2, Sprite.Area.Height / 2);
         }
 
+        /// <summary>
+        /// Call AFTER physics step
+        /// </summary>
         public void Update()
         {
             Position += Velocity / Settings.FramesPerSecond;
             Sprite.Update();
+            Position = ConvertUnits.ToDisplayUnits(PhysicsBody.Position);
+            Rotation = PhysicsBody.Rotation;
+            PhysicsBody.LinearVelocity = Vector2.Zero;
+            PhysicsBody.AngularVelocity = 0;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="angle">Angle in degrees</param>
         public void Rotate(float angle)
         {
             Rotation += MathHelper.ToRadians(angle) / Settings.FramesPerSecond;
+            PhysicsBody.Rotation = Rotation;
         }
         public void MoveStraight(float distance)
         {
             Vector2 vel = new Vector2((float) (distance * Math.Sin(Rotation)),(float) (-distance * Math.Cos(Rotation)));
-            Position += vel / Settings.FramesPerSecond;            
+            vel /= Settings.FramesPerSecond;
+            PhysicsBody.LinearVelocity = vel;
         }
     }
 }
--- src/dev_adventure/dev_adventure/GameStates/DemoGameState.cs
+++ src/dev_adventure/dev_adventure/GameStates/DemoGameState.cs
@@ -9,6 +9,11 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Factories;
+using FarseerPhysics.SamplesFramework;
+using FarseerPhysics.Common;
 
 namespace DevAdventure
 {
@@ -16,20 +21,30 @@ namespace DevAdventure
     {
         private GameObject bug;
         private GameObject bg;
+        private GameObject feature;
 
         private FloatText floatingText;
+
+        World world = new World(Vector2.Zero);
+        Body boundary;
+
         public DemoGameState()
         {
             //TODO: ResMan.AddTeture2D
             requiredResources.Add(new ResMan.Asset() { Name = "bug", Type = ResMan.Asset.AssetType.TEXTURE_2D });
             requiredResources.Add(new ResMan.Asset() { Name = "checkboard", Type = ResMan.Asset.AssetType.TEXTURE_2D });
+
         }
 
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch batch)
         {
             DrawGameObject(batch, bg);
             DrawGameObject(batch, bug);
-            
+            DrawGameObject(batch, feature);
+
+            batch.DrawString(ResMan.Get<SpriteFont>("default"), bug.Position.ToString(), Vector2.Zero, Color.Red);
+            batch.DrawString(ResMan.Get<SpriteFont>("default"), feature.Position.ToString(), Vector2.Zero, Color.Red);
+
             floatingText.DrawAll(batch);
         }
 
@@ -49,21 +64,87 @@ namespace DevAdventure
                 floatingText.Add("Q33NY", InMan.MousePosition, Color.Red);
             }
 
+            world.Step(Settings.FrameTime);
+
+            bug.Update();
+            feature.Update();
+
             floatingText.UpdateAll();
-            LookAt(bg);
+            LookAt(bug);
         }
 
         protected override void AssignResources()
         {
-            bug = new GameObject(new AnimatedSprite(ResMan.Get<Texture2D>("bug")), Vector2.Zero);
+            // bug = new GameObject(new AnimatedSprite(ResMan.Get<Texture2D>("bug")), Vector2.Zero);
             bg = new GameObject(new AnimatedSprite(ResMan.Get<Texture2D>("checkboard")), Vector2.Zero);
             floatingText = new FloatText(ResMan.GetResource<SpriteFont>("default"));
+
+            world = new World(Vector2.Zero);
+            bug = CreatePhysicsObject(new AnimatedSprite(ResMan.Get<Texture2D>("bug")), Vector2.Zero);
+            feature = CreatePhysicsObject(new AnimatedSprite(ResMan.Get<Texture2D>("bug")), new Vector2(400, 400));
+
+            bug.PhysicsBody.Mass = 5;
+            feature.PhysicsBody.Mass = 10;
+
+            bug.PhysicsBody.OnCollision += new OnCollisionEventHandler(PhysicsBody_OnCollision);
+            /*
+            var bounds = new Vertices(4);
+            bounds.Add(ConvertUnits.ToSimUnits(new Vector2(-500, -500)));
+            bounds.Add(ConvertUnits.ToSimUnits(new Vector2(1920, 0)));
+            bounds.Add(ConvertUnits.ToSimUnits(new Vector2(1920, 1080)));
+            bounds.Add(ConvertUnits.ToSimUnits(new Vector2(0, 1080)));*/
+            /*
+            boundary = new Body(world);
+            boundary.CreateFixture(new CircleShape(ConvertUnits.ToSimUnits(10), 0));
+            boundary.Position = ConvertUnits.ToSimUnits(new Vector2(400, 400));
+          /*  boundary.CollisionCategories = Category.All;
+            boundary.CollidesWith = Category.All;
+            boundary.BodyType = BodyType.Dynamic;*/
+        }
+
+        bool PhysicsBody_OnCollision(Fixture fixtureA, Fixture fixtureB, FarseerPhysics.Dynamics.Contacts.Contact contact)
+        {
+            /* System.Windows.Forms.MessageBox.Show(feature.Position.ToString() + " " + bug.Position.ToString());
+             System.Windows.Forms.MessageBox.Show(feature.PhysicsBody.Position.ToString() + " " + bug.PhysicsBody.Position.ToString());
+             */
+            return true;
         }
 
         public override void Activate(object obj)
         {
             if (!HandleResources())
                 return;
+        }
+
+        private GameObject CreatePhysicsObject(AnimatedSprite animated_sprite, Vector2 pos, float rot = 0.0f)
+        {
+            GameObject obj = new GameObject(animated_sprite, pos, rot);
+            obj.PhysicsBody = BodyFactory.CreateBody(world);
+            //obj.PhysicsFixture = obj.PhysicsBody.CreateFixture(new CircleShape(ConvertUnits.ToSimUnits(animated_sprite.Area.Width / 2), 0f));
+            /*obj.PhysicsFixture = obj.PhysicsBody.CreateFixture(
+                new FarseerPhysics.Collision.Shapes.LoopShape(
+                new Vertices(
+                    new Vector2[] { new Vector2(0,0),
+                        new Vector2(0, animated_sprite.Area.Width),
+                        new Vector2(animated_sprite.Area.Height),
+                        new Vector2(animated_sprite.Area.Width),
+                        new Vector2(animated_sprite.Area.Width, 0)})));*/
+
+            var q = new PolygonShape(1);/*
+            q.SetAsBox(ConvertUnits.ToSimUnits(animated_sprite.Area.Width / 2),
+                ConvertUnits.ToSimUnits( animated_sprite.Area.Height / 2),
+                ConvertUnits.ToSimUnits(new Vector2(animated_sprite.Area.Height / 2,
+                    animated_sprite.Area.Height / 2)), 0);*/
+
+            q.SetAsBox(ConvertUnits.ToSimUnits(animated_sprite.Area.Width / 2),
+                ConvertUnits.ToSimUnits(animated_sprite.Area.Height / 2));
+            obj.PhysicsFixture = obj.PhysicsBody.CreateFixture(q);
+
+            obj.PhysicsBody.BodyType = BodyType.Dynamic;
+            obj.PhysicsBody.Position = ConvertUnits.ToSimUnits(pos);
+            obj.PhysicsBody.Rotation = MathHelper.ToRadians(rot);
+            obj.PhysicsBody.Friction = 50;
+            return obj;
         }
     }
 }
